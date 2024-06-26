eb049342f7d6acee2ed6afb4b38bd4d0c2ccb042
Mayank Yadav <mayank4code@gmail.com>
2024-06-26 17:50:15 +0530

    trying to deploy

--- src/api/user.js
+++ src/api/user.js
@@ -69,7 +69,7 @@ router.post("/login", userAuth, async (req,res)=>{
             isAdmin: user.role===2?true:false
         };
         const token = jwt.sign(data, process.env.JWT_SECRET);
-
+        console.log(req.body);
         res.status(200).json({success: true, message: "Logged in successfully", token});
 
     } catch(err){
